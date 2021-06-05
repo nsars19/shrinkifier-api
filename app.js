54247e46bb79604229914baed6dbe18060c8f3fc
Nick S <nsardonini@gmail.com>
2021-06-04 20:36:35 -0400

    add middleware to route

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const logger = require("morgan");
+const cors = require("cors");
+const upload = require("./utils/upload");
+const { compress, attachNewFiles, rmdirs } = require("./utils/compress");
+
 const app = express();
 
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.post("/", upload, compress, attachNewFiles, rmdirs, (req, res) => {
+  res.send({ data: req.files });
+});
 
 module.exports = app;
