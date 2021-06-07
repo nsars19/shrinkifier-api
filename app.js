9004bc6faea0c3879f27ad0f2fcd2b35c6f44366
Nick S <nsardonini@gmail.com>
2021-06-07 15:49:51 -0400

    add image processing middleware to app route

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const upload = require("./utils/upload");
-const { compress, attachNewFiles, rmdirs } = require("./utils/compress");
-
+const { processingHandler } = require("./utils/compress");
 const app = express();
 
 app.use(logger("dev"));
@@ -11,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/", upload, compress, attachNewFiles, rmdirs, (req, res) => {
-  res.send({ data: req.files });
+app.post("/", upload, processingHandler, (req, res) => {
+  res.send({ zipFile: req.zipFile });
 });
 
 module.exports = app;
