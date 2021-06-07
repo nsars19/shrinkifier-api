e0ce28a1335894fcd1fdd856756e536a6b857add
Nick S <nsardonini@gmail.com>
2021-06-07 15:49:24 -0400

    add middleware stack for processing images

--- utils/compress.js
+++ utils/compress.js
@@ -1,37 +1,59 @@
-const { processFromDir, rmdir } = require("shrinkifier");
 const fs = require("fs");
+const sharp = require("sharp");
+const AdmZip = require("adm-zip");
+const zip = new AdmZip();
+const { resolve } = require("path");
 
-exports.compress = (req, res, next) => {
-  processFromDir({
-    start: "./tmp/unprocessed/",
-    finish: "./tmp/processed/",
+const [start, finish] = ["./tmp/unprocessed/", "./tmp/processed/"];
+
+async function shrinkifier(images, startPath, endPath) {
+  if (!fs.existsSync(endPath)) {
+    fs.mkdirSync(endPath, { recursive: true });
+  }
+
+  for (const image of images) {
+    await sharp(startPath + image)
+      .jpeg({ quality: 60 })
+      .toFile(`${endPath}${image.split(".")[0]}.jpeg`);
+  }
+}
+
+const compressImages = async (req, res, next) => {
+  const files = fs.readdirSync(start, (err, data) => {
+    if (err) console.error(err);
+    else return data;
   });
+
+  await shrinkifier(files, start, finish);
 
   next();
 };
 
-exports.attachNewFiles = (req, res, next) => {
-  fs.readdir("./tmp/processed/", (err, data) => {
-    req.newFiles = data;
-  });
+const compressDirectory = async (req, res, next) => {
+  const path = resolve(finish);
+
+  zip.addLocalFolder(path);
+  req.zipFile = zip.toBuffer();
 
   next();
 };
 
-exports.rmdirs = () => {
-  ["processed", "unprocessed"].forEach((path) => {
-    if (fs.existsSync("./tmp/" + path)) {
-      fs.readdir("./tmp/" + path, (err, files) => {
-        if (err) {
-          console.error(err);
-        } else {
-          files.forEach((file) => {
-            fs.unlink(`./tmp/${path}/${file}`, (err, data) => {
-              if (err) console.error(err);
-            });
-          });
-        }
-      });
-    }
-  });
+const unlinkFiles = async (req, res, next) => {
+  const startNames = fs.readdirSync(start);
+  const finishNames = fs.readdirSync(finish);
+
+  startNames.forEach((file) =>
+    fs.unlink(resolve(start) + "/" + file, (err) => {
+      if (err) console.error(err);
+    })
+  );
+  finishNames.forEach((file) =>
+    fs.unlink(resolve(finish) + "/" + file, (err) => {
+      if (err) console.error(err);
+    })
+  );
+
+  next();
 };
+
+exports.processingHandler = [compressImages, compressDirectory, unlinkFiles];
