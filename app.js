a873706ed0d291937e1105c2dc61659c4b7cf6b1
Nick S <nsardonini@gmail.com>
2021-06-08 18:07:34 -0400

    unlink zip file after sending

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const upload = require("./utils/upload");
-const { processingHandler } = require("./utils/compress");
+const { processingHandler, unlinkZip } = require("./utils/compress");
 const app = express();
 
 app.use(logger("dev"));
@@ -10,8 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/", upload, processingHandler, (req, res) => {
-  res.send({ zipFile: req.zipFile });
+app.post("/", upload, processingHandler, async (req, res) => {
+  res.on("finish", () => unlinkZip(req));
+  res.sendFile(req.zipFilePath);
 });
 
 module.exports = app;
